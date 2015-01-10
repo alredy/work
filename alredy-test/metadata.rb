bbbd5bca3312cc16ede6b1abc421cac2f758fef4
Andrew Polyyakov <highway.cry@gmail.com>
2015-01-10 13:26:53 +0200

    create env part1

--- alredy-test/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'alredy-test'
-maintainer       'Grammarly, Inc.'
-maintainer_email 'highway.cry@gmail.com'
-license          'MIT'
-description      'Installs/Configures alredy-test'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends         'java'
--- alredy-test/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-puts "__1__"
-#puts roles.each_slice(1)
-puts "__2__"
-#puts groupname.each_slice(1)
-puts "__3__"
-#puts roles
